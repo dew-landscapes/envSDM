dew-landscapes/envSDM

commit 6113ef7bea628c769e7b197d7ce7a9da953680bc

Catch and fix cases with presences on predictors but outside predict boundary

R/prep_sdm.R
@@ -224,6 +224,17 @@
                             , write_log = TRUE
                             )
 
+        # prep$presence clip to predict_boundary ---------
+        # catch some cases where there are presence records on the predictors
+        # but outside the predict boundary (so there are no background points
+        # around those presences).
+        prep$presence <- prep$presence %>%
+          sf::st_as_sf(coords = c("x", "y")) %>%
+          sf::st_transform(crs = sf::st_crs(prep$predict_boundary)) %>%
+          sf::st_intersection() %>%
+          sf::st_set_geometry(NULL)
+
+
         # folds adj -------
 
         # aiming for at least 'min_fold_n' presences in each fold

commit 44ee12bca63296aff88f8caa89a29276182799ab

Fix bug introduced in 6113ef7

R/prep_sdm.R
@@ -229,10 +229,15 @@
         # but outside the predict boundary (so there are no background points
         # around those presences).
         prep$presence <- prep$presence %>%
-          sf::st_as_sf(coords = c("x", "y")) %>%
-          sf::st_transform(crs = sf::st_crs(prep$predict_boundary)) %>%
-          sf::st_intersection() %>%
-          sf::st_set_geometry(NULL)
+          sf::st_as_sf(coords = c("x", "y")
+                       , crs = sf::st_crs(predictors)
+                       ) %>%
+          sf::st_filter(prep$predict_boundary %>%
+                          sf::st_transform(crs = sf::st_crs(predictors))
+                        ) %>%
+          sf::st_coordinates()
+
+        base::colnames(prep$presence) <- c("x", "y")
 
 
         # folds adj -------
@@ -395,7 +400,23 @@
             terra::stretch(1, stretch_value) %>%
             terra::subst(NA,0)
 
-          if(FALSE) terra::plot(target_density)
+          if(FALSE) {
+
+            terra::plot(predictors[[1]])
+            terra::plot(target_density, add = TRUE)
+
+            ps <- prep$presence %>%
+              tibble::as_tibble() %>%
+              sf::st_as_sf(coords = c("x", "y"), crs = sf::st_crs(predictors)) %>%
+              terra::vect()
+
+            terra::plot(ps, add = TRUE)
+
+            terra::plot(prep$predict_boundary %>% terra::vect()
+                        , add = TRUE
+                        )
+
+          }
 
           terra::writeRaster(target_density
                              , density_file