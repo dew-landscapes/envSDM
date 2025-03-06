728d4be61006f3516713ffaef8852c49aa694726
Acanthiza <nigel.willoughby@sa.gov.au>
2025-03-06 10:35:04 +1030

    Add threshold metrics into metrics_df

--- data-raw/sdm_metrics.R
+++ data-raw/sdm_metrics.R
@@ -1,39 +1,28 @@
 # focus on getting the presences right
-  sdm_metrics <- tibble::tibble(metric = c(
-  # predicts::pa_evaluate
-    "TPR", "TNR"
-    , "FPR", "FNR"
-    , "PPP", "NPP"
-    , "MCR", "auc_po"
-    , "ODP", "kappa"
-  # flexsdm::fine_eval
-    , "CBI_rescale", "IMAE"
-    , "auc_po_flexsdm", "CBI"
-    )
-    , high_good = c(T, T
-                    , F, F
-                    , T, T
-                    , F, T
-                    , T, T
-                    , T, T
-                    , T, T
-                    )
-  , is_thresh = c(T, T
-                  , T, T
-                  , T, T
-                  , F, F
-                  , F, F
-                  , F, F
-                  , F, F
+  sdm_metrics <- tibble::tribble(
+    ~metric, ~high_good, ~is_thresh
+    # predicts::pa_evaluate
+    , "TPR", T, T
+    , "TNR", T, T
+    , "FPR", F, T
+    , "FNR", F, T
+    , "PPP", T, T
+    , "NPP", T, T
+    , "MCR", F, T
+    , "max_spec_sens", T, T
+    , "no_omission", T, T
+    , "equal_prevalence", T, T
+    , "equal_sens_spec", T, T
+    , "auc_po", T, F
+    , "ODP", T, F
+    # generated
+    , "or10", T, T
+    # flexsdm::fine_eval
+    , "CBI", T, F
+    , "CBI_rescale", T, F
+    , "IMAE", T, F
+    , "auc_po_flexsdm", T, F
+    ) |>
+    dplyr::mutate(within_mets = T
+                  , across_mets = T
                   )
-    , summary_mets = c(F, F
-                       , F, F
-                       , F, F
-                       , F, T
-                       , F, F
-                       , T, T
-                       , F, F
-                       )
-    , within_mets = T
-    , across_mets = T
-    )
