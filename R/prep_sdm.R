dew-landscapes/envSDM

commit 302f813238e0292f0f5a88b5c795cda9c3b9d94e

Improved checking of correlation in (spatial) folds between repeats

R/prep_sdm.R
@@ -63,6 +63,9 @@
 #' by this value before being passed to the `block_dist` argument of
 #' `blockCV::cv_spatial()`. If using repeated cross validation, `block_div`
 #' must be of the same length as `1:repeats`.
+#' @param max_block_corr Numeric. Maximum correlation allowed between the folds
+#' of any two spatial blocks before one of the correlated blocks will be set to
+#' non-spatial and the folds reallocated.
 #' @param min_fold_n Numeric. Sets both minimum number of presences, and,
 #' by default, the minimum number of presences required for a model.
 #' @param stretch_value Numeric. Stretch the density raster to this value.
@@ -142,6 +145,7 @@
                        , spatial_folds = TRUE
                        , repeats = 1
                        , block_div = seq(5, by = 2, length.out = repeats)
+                       , max_block_corr = 0.8
                        , min_fold_n = 8
                        , hold_prop = 0.3
                        , stretch_value = 10
@@ -990,19 +994,6 @@
 
             }
 
-            ### spatial cv identical --------
-            prep_block_corr <- stats::cor(tibble::as_tibble(prep$training$blocks, .name_repair = "unique"))
-
-            change_to_non_spatial <- caret::findCorrelation(prep_block_corr
-                                                            , cutoff = 0.999999999
-                                                            )
-
-            if(sum(change_to_non_spatial, na.rm = TRUE)) {
-
-              prep$training$spatial_folds[change_to_non_spatial] <- FALSE
-
-            }
-
             ### too few p --------
             prep$training <- prep$training |>
               dplyr::filter(purrr::map_lgl(error, \(x) is.null(x))) |>
@@ -1014,6 +1005,19 @@
                                                              , \(x) length(unique(x)) > 1
                                                              )
                             )
+
+            ### spatial cv identical --------
+            prep_block_corr <- stats::cor(tibble::as_tibble(prep$training$blocks, .name_repair = "unique"))
+
+            change_to_non_spatial <- caret::findCorrelation(prep_block_corr
+                                                            , cutoff = max_block_corr
+                                                            )
+
+            if(sum(change_to_non_spatial, na.rm = TRUE)) {
+
+              prep$training$spatial_folds[change_to_non_spatial] <- FALSE
+
+            }
 
           }
 

commit 0bff11268ca6cf9789533e2469a85f41b1448d5b

Fix bug introduced by 302f813

R/prep_sdm.R
@@ -1007,15 +1007,19 @@
                             )
 
             ### spatial cv identical --------
-            prep_block_corr <- stats::cor(tibble::as_tibble(prep$training$blocks, .name_repair = "unique"))
+            if(length(prep$training$blocks) > 1) {
 
-            change_to_non_spatial <- caret::findCorrelation(prep_block_corr
-                                                            , cutoff = max_block_corr
-                                                            )
+              prep_block_corr <- stats::cor(tibble::as_tibble(prep$training$blocks, .name_repair = "unique"))
 
-            if(sum(change_to_non_spatial, na.rm = TRUE)) {
+              change_to_non_spatial <- caret::findCorrelation(prep_block_corr
+                                                              , cutoff = max_block_corr
+                                                              )
 
-              prep$training$spatial_folds[change_to_non_spatial] <- FALSE
+              if(sum(change_to_non_spatial, na.rm = TRUE)) {
+
+                prep$training$spatial_folds[change_to_non_spatial] <- FALSE
+
+              }
 
             }
 