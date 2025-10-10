74191a1b012c5301051ac82961d21d737b2fcc11
Joel Allan <JAllan@env.sa.gov.au>
2025-10-10 15:07:52 +1030

    fix for non-spatial folds instead of spatial folds being created

--- R/prep_sdm.R
+++ R/prep_sdm.R
@@ -1060,7 +1060,7 @@
                                                      )
                                                 , \(x, y, z) {
 
-                                                  if(x) y else z
+                                                  if(!x) y else z
 
                                                 }
                                                 )
