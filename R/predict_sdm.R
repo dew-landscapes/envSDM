dew-landscapes/envSDM

commit c9d7ee75905a73a873e78967cdd946f929bae908

Improve, and fix bugs in, logs

R/predict_sdm.R
@@ -121,7 +121,8 @@ predict_sdm <- function(prep
   }
 
   ### log --------
-  log <- paste0(this_taxa
+  log <- paste0(if(file_exists(log_file)) paste0(readr::read_lines(log_file), "\n\n")
+                , this_taxa
                 , "\npredict process started at "
                 , start_time
                 )

R/tune_sdm.R
@@ -433,7 +433,7 @@
 
               message(m)
 
-              prep$log <- paste0(prep$log
+              tune$log <- paste0(tune$log
                                  , "\n"
                                  , m
                                  )
@@ -474,7 +474,7 @@
 
               message(m)
 
-              prep$log <- paste0(prep$log
+              tune$log <- paste0(tune$log
                                  , "\n"
                                  , m
                                  )
@@ -485,7 +485,7 @@
               dplyr::filter(purrr::map_lgl(e, \(x) ! is.null(x))) %>%
               {if(keep_model) (.) %>% dplyr::select(! dplyr::where(is.list), m, e) else (.) %>% dplyr::select(! dplyr::where(is.list), e)}
 
-            prep$log <- paste0(prep$log
+            tune$log <- paste0(tune$log
                                , "\n"
                                , "maxnet tune finished in: "
                                , round(difftime(Sys.time(), start_maxnet, units = "mins"), 2)
@@ -529,7 +529,7 @@
                             ) %>%
               {if(keep_model) (.) %>% dplyr::select(! dplyr::where(is.list), m, e) else (.) %>% dplyr::select(! dplyr::where(is.list), e)}
 
-            prep$log <- paste0(prep$log
+            tune$log <- paste0(tune$log
                                , "\n"
                                , "envelope tune finished in: "
                                , round(difftime(Sys.time(), start_envelope, units = "mins"), 2)
@@ -658,7 +658,7 @@
 
               message(m)
 
-              prep$log <- paste0(prep$log
+              tune$log <- paste0(tune$log
                                  , "\n"
                                  , m
                                  )
@@ -684,7 +684,7 @@
                   dplyr::select(! dplyr::where(is.list), e)
               }
 
-            prep$log <- paste0(prep$log
+            tune$log <- paste0(tune$log
                                , "\n"
                                , "rf tune finished in: "
                                , round(difftime(Sys.time(), start_rf, units = "mins"), 2)
@@ -703,7 +703,7 @@
 
     } else {
 
-      prep$log <- paste0(prep$log
+      tune$log <- paste0(tune$log
                          , "\n"
                          , "tune abandoned"
                          )
@@ -777,7 +777,7 @@
 
           if(!best_run) {
 
-            prep$log <- paste0(prep$log
+            tune$log <- paste0(tune$log
                                , "\n"
                                , "based on the product of metrics: "
                                , stringr::str_flatten_comma(metrics_df$metric[metrics_df$summary_mets])

commit f8d63b7e134ad8e9bb3016701a495b6cf23a7f8e

fix bug introduced in c9d7ee7

R/predict_sdm.R
@@ -121,7 +121,7 @@ predict_sdm <- function(prep
   }
 
   ### log --------
-  log <- paste0(if(file_exists(log_file)) paste0(readr::read_lines(log_file), "\n\n")
+  log <- paste0(if(file.exists(log_file)) paste0(readr::read_lines(log_file), "\n\n")
                 , this_taxa
                 , "\npredict process started at "
                 , start_time