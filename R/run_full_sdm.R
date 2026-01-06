dew-landscapes/envSDM

commit dbc9221df88050879f63f614a7dd62700cac8220

If only 1 'tune', don't run again in run_full_model

R/run_full_sdm.R
@@ -102,14 +102,21 @@
       # tune_args------
       row_id <- 1
 
-      tune_args <- tune$tune_mean %>%
-        dplyr::filter(tunes == max(tunes)) |>
+      tune_args <- tune$tune_mean |>
+        dplyr::filter(tunes >= quantile(tunes, probs = 0.9)) |> # don't want too many tunes to have failed
         dplyr::arrange(desc(!!rlang::ensym(use_metric)))
-
 
       full_run_tune <- NULL
 
-      if(nrow(tune_args)) {
+      if(all(tune$finished
+             , max(unique(tune_args$tunes)) == 1
+             )
+         ){
+
+        # If only 1 repeat/block, no need to run again
+        full_run_tune <- tune
+
+      } else if(nrow(tune_args)) {
 
         # mod --------
 

R/tune_sdm.R
@@ -483,7 +483,7 @@
 
             tune$tune_maxnet <- tune$tune_maxnet %>%
               dplyr::filter(purrr::map_lgl(e, \(x) ! is.null(x))) %>%
-              {if(keep_model) (.) %>% dplyr::select(! dplyr::where(is.list), m, e) else (.) %>% dplyr::select(! dplyr::where(is.list), e)}
+              {if(any(keep_model, nrow(tune$tune_maxnet == 1))) (.) %>% dplyr::select(! dplyr::where(is.list), m, e) else (.) %>% dplyr::select(! dplyr::where(is.list), e)}
 
             tune$log <- paste0(tune$log
                                , "\n"
@@ -527,7 +527,7 @@
                                                                         )
                                               )
                             ) %>%
-              {if(keep_model) (.) %>% dplyr::select(! dplyr::where(is.list), m, e) else (.) %>% dplyr::select(! dplyr::where(is.list), e)}
+              {if(any(keep_model, nrow(tune$tune_envelope == 1))) (.) %>% dplyr::select(! dplyr::where(is.list), m, e) else (.) %>% dplyr::select(! dplyr::where(is.list), e)}
 
             tune$log <- paste0(tune$log
                                , "\n"
@@ -679,7 +679,7 @@
                                             , .progress = "evaluate rf"
                                             )
                             ) %>%
-              {if(keep_model) (.) %>%
+              {if(any(keep_model, nrow(tune$tune_rf == 1))) (.) %>%
                   dplyr::select(! dplyr::where(is.list), m, e) else (.) %>%
                   dplyr::select(! dplyr::where(is.list), e)
               }

commit 5d43be397b555b81550973897a1d495dded20424

Fix another bug introduced in dbc9221

R/run_full_sdm.R
@@ -114,7 +114,9 @@
          ){
 
         # If only 1 repeat/block, no need to run again
-        full_run_tune <- tune
+        full_run <- tune
+
+        full_run$finished <- TRUE
 
       } else if(nrow(tune_args)) {
 